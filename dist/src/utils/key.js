1e3058f3556ae19323c7efb22b5178e89e684696
Eric <niyonkurufamous@gmail.com>
2022-06-21 00:07:59 +0200

    adding feature in heroku

--- dist/src/utils/key.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP_SECRET = void 0;
-exports.APP_SECRET = process.env.JWT_SECRET || "yegotooo";
-//# sourceMappingURL=key.js.map
